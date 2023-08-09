7c31e57ea922117363af09a869844cf89e495f75
TM-59 <68103513+TM-59@users.noreply.github.com>
2023-08-09 14:34:59 +0900

    Add files via uploading

---
+++ dev/check_module_imported.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  4 14:59:12 2023
+
+@author: taichi.mitsuhashi
+"""
+
+import sys
+import subprocess
+from bs4 import BeautifulSoup
+
+if "bs4" in sys.modules:
+    print("module already imported")
+else:
+    print("module not imported")
+
+
+
